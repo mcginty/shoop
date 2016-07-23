99d497b3192718b42e397d6670dad762fefcabcb
Jake McGinty <me@jake.su>
2016-07-23 20:29:33 +0700

    switch to udt because life is too short

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,23 @@
 #[macro_use]
 extern crate log;
 extern crate env_logger;
-extern crate utp;
 extern crate getopts;
 extern crate daemonize;
+extern crate udt;
 
 use daemonize::{Daemonize};
 use std::process;
 use std::process::Command;
 use std::thread;
-use std::net::{SocketAddr, SocketAddrV4, Ipv4Addr, UdpSocket};
+use std::net::{SocketAddr, SocketAddrV4, Ipv4Addr};
 use std::net;
 use std::str;
 use std::env;
 use std::fs::File;
+use std::str::FromStr;
 use std::path::Path;
 use getopts::Options;
+use udt::*;
 
 fn print_usage(program: &str, opts: Options) {
     let brief = format!("Usage: {} [options] REMOTE-LOCATION", program);
@@ -24,7 +26,6 @@ fn print_usage(program: &str, opts: Options) {
 }
 
 fn main() {
-    use utp::{UtpStream, UtpListener, UtpSocket};
     use std::io::{stdin, stdout, stderr, Read, Write};
 
     // This example may run in either server or client mode.
@@ -68,24 +69,26 @@ fn main() {
 
     match mode {
         Mode::Server => {
-            // Create a listening stream
-            let addr = "0.0.0.0:55000";
-            let mut stream = UdpSocket::bind(&addr).expect("Error binding stream");
+            let sock = UdtSocket::new(SocketFamily::AFInet, SocketType::Stream).unwrap();
+            sock.bind(SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::from_str("0.0.0.0").unwrap(), 55000)));
+            let my_addr = sock.getsockname().unwrap();
+            println!("Server bound to {:?}", my_addr);
+
+            sock.listen(5).unwrap();
+
+            let (mut stream, peer) = sock.accept().unwrap();
+            println!("Received new connection from peer {:?}", peer);
 
             let mut f = File::open(input).unwrap();
-            println!("{}", addr);
 
             let daemonize = Daemonize::new();
 
             let mut writer = stdout();
-            let _ = writeln!(&mut stderr(), "Serving on {}", addr);
-
-            // Create a reasonably sized buffer
-            let mut payload = vec![0; 1024 * 1024];
+            // let _ = writeln!(&mut stderr(), "Serving on {}", addr);
 
             let mut startmagic = vec![0; 1];
-            let (amount_read, sender_address) = stream.recv_from(&mut startmagic).unwrap();
-            println!("got {} magic byte(s) from {:?}.", amount_read, sender_address);
+            let amount_read = stream.recv(&mut startmagic, 1).unwrap();
+            println!("got {} magic byte(s).", amount_read);
 
             // match daemonize.start() {
             //     Ok(_) => { let _ = writeln!(&mut stderr(), "daemonized"); }
@@ -96,9 +99,9 @@ fn main() {
             // Create a reasonably sized buffer
             let mut payload = vec![0; 1300];
             loop {
-                match stream.send_to(&payload, sender_address) {
+                match stream.send(&payload) {
                     Ok(written) => { print!("."); }
-                    Err(e) => { panic!("{}", e); }
+                    Err(e) => { panic!("{:?}", e); }
                 }
             }
             // loop {
@@ -141,20 +144,27 @@ fn main() {
             //
             // // Create a stream and try to connect to the remote address
             // println!("shoop server told us to connect to {}", udp_addr);
-            let mut stream = UdpSocket::bind("0.0.0.0:0").expect("Error connecting to remote peer");
-            println!("initted.");
+            let sock = UdtSocket::new(SocketFamily::AFInet, SocketType::Stream).unwrap();
+            let addr: SocketAddr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::from_str("144.76.81.4").unwrap(), 55000));
+            match sock.connect(addr) {
+                Ok(()) => {
+                    println!("connected!");
+                },
+                Err(e) => {
+                    panic!("errrrrrrr {:?}", e);
+                }
+            }
 
             // let mut f = File::create("outfile").unwrap();
             // println!("created file.");
             // Create a reasonably sized buffer
             let mut payload = vec![0; 1024 * 1024];
-            stream.connect("144.76.81.4:55000");
-            stream.send(b"\x01");
+            sock.send(b"\x01");
             println!("write magic byte.");
 
             let mut total = 0;
             loop {
-                match stream.recv(&mut payload) {
+                match sock.recv(&mut payload, 1024 * 1024) {
                     Ok(0) => {
                         println!("EOF");
                         break
