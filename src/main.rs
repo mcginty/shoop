82cba246674734fdb213a846e812d5d85681cb99
Jake McGinty <me@jake.su>
2018-09-09 16:34:18 +0000

    clap -> structopt

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,69 @@
 // #![feature(alloc_system)]
 // extern crate alloc_system;
 
-#[macro_use]
-extern crate log;
+#[macro_use] extern crate log;
+extern crate structopt;
 extern crate shoop;
-#[macro_use]
-extern crate clap;
 
-use clap::App;
-use std::str;
+use structopt::StructOpt;
 use shoop::{ShoopLogger, ShoopMode, LogVerbosity, TransferMode,
             Target, Server, Client};
 use shoop::connection::PortRange;
 
-const DEFAULT_PORT_RANGE: &'static str = "55000-55050";
+#[derive(StructOpt, Debug)]
+#[structopt(name = "shoop")]
+struct Opt {
+    #[structopt(short = "d", long = "debug")]
+    debug: bool,
+
+    #[structopt(short = "f", long = "force")]
+    force: bool,
+
+    #[structopt(short = "s", long = "server")]
+    server: bool,
+
+    #[structopt(short = "p", long = "port-range", default_value = "55000-55050")]
+    port_range: String,
+
+    #[structopt(name = "SOURCE")]
+    source: String,
+
+    #[structopt(name = "DEST", default_value = ".")]
+    dest: String,
+}
 
 fn main() {
-    let matches = App::new("shoop")
-        .version(crate_version!())
-        .author(crate_authors!())
-        .about("Shoop is a ultrafast, (hopefully) secure file transfer tool, that is (hopefully) ideal for transferring large files.")
-        .args_from_usage(
-            "-p --port-range=[RANGE] 'server UDP port range (ex. \"55000-55050\")'
-             -f --force              'overwrite the output file if it exists'
-             -d --debug              'output verbose debug information'
-             -s --server             'server mode'
-             -r --receive            'receive mode (server mode only)'
-             <SOURCE>                'input \"host:path\", just like scp (ex. \"myseedbox.com:~/downloads/linux.iso\")'
-             [DEST]                  'optional output path (directory or file, ex. \".\" or \"~/isos\" or \"~/isos/totallynotporn.iso\")'")
-        // .subcommand(SubCommand::with_name("server")
-        //             .about("Shoop server mode.")
-        //             .args_from_usage(
-        //                 "-r --receive 'receive mode (client -> server)'"))
-        .get_matches();
+    let opt = Opt::from_args();
 
-    let raw_source = matches.value_of("SOURCE").unwrap();
-
-    let mode = match matches.is_present("server") {
+    let mode = match opt.server {
         true => ShoopMode::Server,
         _    => ShoopMode::Client,
     };
 
-    let verbosity = match matches.is_present("debug") {
+    let verbosity = match opt.debug {
         true => LogVerbosity::Debug,
         _    => LogVerbosity::Normal,
     };
 
-    let port_range = {
-        let range = matches.value_of("port-range")
-            .unwrap_or_else(|| DEFAULT_PORT_RANGE.into());
-        PortRange::from(range).unwrap()
-    };
+    let port_range = PortRange::from(&opt.port_range).unwrap();
 
     ShoopLogger::init(mode, verbosity).expect("Error starting shoop logger.");
 
     match mode {
         ShoopMode::Server => {
-            let transfer_mode = match matches.is_present("receive") {
-                true => TransferMode::Receive,
-                _    => TransferMode::Send,
-            };
+            let transfer_mode = TransferMode::Send;
 
-            if let Ok(mut server) = Server::new(port_range, &raw_source) {
+            if let Ok(mut server) = Server::new(port_range, &opt.source) {
                 server.start(transfer_mode);
                 info!("exiting.");
             }
         },
         ShoopMode::Client => {
-            let raw_dest = matches.value_of("DEST")
-                .unwrap_or_else(|| ".".into());
-
-            let source = Target::from(raw_source.to_owned());
-            let dest = Target::from(raw_dest.to_owned());
+            let source = Target::from(opt.source.clone());
+            let dest = Target::from(opt.dest.clone());
 
             match Client::new(source, dest, port_range) {
-                Ok(mut client) => client.start(matches.is_present("force")),
+                Ok(mut client) => client.start(opt.force),
                 Err(e) => error!("{}", e),
             }
         }
